6c374cc987c543a0c607657abf89523bb167a7a4
mattdepillis <mattdepillis@gmail.com>
2022-04-01 17:09:36 -0400

    first workings of spotify api integration -- need to fix fetchData call to /me/ endpoint

--- pages/media.js
+++ pages/media.js
@@ -10,25 +10,32 @@ import { AppContext } from '../global-state/AppContext'
 import { fetchSidebarOptions } from '../notion-api/sidebar'
 import { setupNotionAPIClients } from '../notion-api/utils'
 import { resolveNotionPage } from '../lib/resolve-notion-page'
+import { getTopRead } from '../lib/spotify/get-top-read'
 
 export const getStaticProps = async () => {
   const { officialNotionClient, notionClient } = setupNotionAPIClients()
 
   const sidebarOptions = await fetchSidebarOptions(officialNotionClient, process.env.PORTFOLIO_HUB_PAGE_ID)
 
+  // TODO: keep tabs with react-notion-x issues with linked collections
   const mediaPageData = await resolveNotionPage(process.env.MEDIA_PAGE_ID)
+
+  // TODO: get spotify api data here
+  const topSpotifyData = await getTopRead()
 
   return {
     props: {
       sidebarOptions,
-      mediaPageData
+      mediaPageData,
+      topSpotifyData
     }
   }
 }
 
 const MediaPage = ({
   sidebarOptions,
-  mediaPageData
+  mediaPageData,
+  topSpotifyData
 }) => {
   const { setSidebarOptions } = useContext(AppContext)
   const [mediaPage, setMediaPage] = useState(undefined)
@@ -41,6 +48,10 @@ const MediaPage = ({
     console.log('m', mediaPageData)
     setMediaPage(mediaPageData)
   }, [mediaPageData])
+
+  useEffect(() => {
+    console.log('topSpotifyData', topSpotifyData)
+  }, [topSpotifyData])
 
   return (
     <Fragment>
