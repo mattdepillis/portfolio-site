0d650f8027d2cd70b433b996e342cd3d2a3dfdb2
mattdepillis <mattdepillis@gmail.com>
2022-04-18 17:59:00 -0400

    added initial code for in-site links for os menu

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import cs from 'classnames'
+import Link from 'next/link'
 
 import styles from '../styles/custom-css/Footer.module.css'
 
@@ -38,12 +39,48 @@ const socialLinks = [
   }
 ]
 
-/*
-  TODO: transform this into an OS-like applications bar for my site -- support each subpage
-*/
+const siteLinks = [
+  {
+    name: 'home',
+    href: '/',
+    title: 'mattdepillis home',
+    icon: '👨🏼‍💻'
+  },
+  {
+    name: 'writing',
+    href: '/writing',
+    title: 'mattdepillis writing',
+    icon: '🖋'
+  },
+  {
+    name: 'media',
+    href: '/media',
+    title: 'mattdepillis media',
+    icon: '📀'
+  }
+]
+
 export const Footer = () =>
   <div className={styles.container}>
     <footer className={styles.pageAside}>
+      {siteLinks.map((page) => (
+        <Link href={page.href}>
+          <a
+            className={cs(styles.action, styles[page.name])}
+            key={page.name}
+            title={page.title}  
+          >
+            <div className={styles.actionBg}>
+              <div className={styles.actionBgPane} />
+            </div>
+
+            <div className={styles.actionBg}>
+              <p>{page.icon}</p>
+            </div>
+          </a>
+        </Link>
+      ))}
+      <div className={styles.divider}></div>
       {socialLinks.map((action) => (
         <a
           className={cs(styles.action, styles[action.name])}
