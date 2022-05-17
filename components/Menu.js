b221f76646c7b22ea00dd184cbe84dd173aed887
mattdepillis <mattdepillis@gmail.com>
2022-05-17 10:31:00 -0400

    got dot to show up under activeRoute subpage

--- components/Menu.js
+++ components/Menu.js
@@ -1,49 +1,64 @@
 import cs from 'classnames'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import styles from '../styles/custom-css/Menu.module.css'
 import { siteLinks, socialLinks } from './Links'
 
 
-export const Menu = () =>
-  <div className={styles.container}>
-    <footer className={styles.pageAside}>
-      {siteLinks.map((page) => (
-        <Link href={page.href}>
-          <a
-            className={cs(styles.action, styles[page.name])}
-            key={page.name}
-            title={page.title}  
-          >
-            <div className={styles.actionBg}>
-              <div className={styles.actionBgPane} />
-            </div>
+export const Menu = () => {
+  const router = useRouter()
 
-            <div className={styles.actionBg}>
-              <p>{page.icon}</p>
-            </div>
-          </a>
-        </Link>
-      ))}
+  console.log(router.pathname)
 
-      <div className={styles.divider}></div>
-      {socialLinks.map((action) => (
-        <a
-          className={cs(styles.action, styles[action.name])}
-          href={action.href}
-          key={action.name}
-          title={action.title}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <div className={styles.actionBg}>
-            <div className={styles.actionBgPane} />
+  return (
+    <div className={styles.container}>
+      <footer className={styles.pageAside}>
+        {siteLinks.map((page) => (
+          <div className={cs(
+            styles.siteLinkContainer,
+            (router.pathname === page.href) && styles.activeRoute)}>
+            <Link href={page.href}>
+              <a
+                className={cs(styles.action, styles[page.name])}
+                key={page.name}
+                title={page.title}  
+              >
+                <div className={styles.page}>
+                  <div className={styles.pagePane} />
+                </div>
+
+                <div className={styles.page}>
+                  <p>{page.icon}</p>
+                </div>
+              </a>
+            </Link>
           </div>
+        ))}
 
-          <div className={styles.actionBg}>{action.icon}</div>
-        </a>
-      ))}
-    </footer>
-  </div>
+        <div className={styles.divider}></div>
+
+        {socialLinks.map((social) => (
+          <div>
+            <a
+              className={cs(styles.action, styles[social.name])}
+              href={social.href}
+              key={social.name}
+              title={social.title}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <div className={styles.social}>
+                <div className={styles.socialPane} />
+              </div>
+
+              <div className={styles.social}>{social.icon}</div>
+            </a>
+          </div>
+        ))}
+      </footer>
+    </div>
+  )
+}
 
 export default Menu
