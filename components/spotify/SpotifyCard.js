859c5ded0f63849418a95f8f869c264cdc349883
mattdepillis <mattdepillis@gmail.com>
2022-05-24 14:28:52 -0400

    split spotify card into own component file

---
+++ components/spotify/SpotifyCard.js
@@ -0,0 +1,53 @@
+import { Card, Grid, Row, Text } from '@nextui-org/react'
+
+const SpotifyCard = ({ i, obj, hovered, section, onMouseOver, onMouseLeave }) =>
+  <Card
+    key={i}
+    className="spotify-card"
+    cover
+    hoverable
+    clickable
+    onClick={() => window.open(`${obj.external_urls.spotify}`, '_blank')}
+    onMouseOver={onMouseOver}
+    onMouseLeave={onMouseLeave}
+  >
+    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }} />
+    <Card.Body>
+      <Card.Image
+        className='spotify-card-image'
+        src={obj.album ? obj.album.images[1].url : obj.images[1].url}
+      />
+    </Card.Body>
+    {hovered &&
+      <Card.Footer
+        blur
+        justify="flex-start"
+        className='spotify-card-footer'
+        css={{
+          position: "absolute",
+          bgBlur: "#0f1114",
+          borderTop: "$borderWeights$light solid $gray700",
+          bottom: 0,
+          zIndex: 1,
+          padding: '6px !important'
+        }}
+      >
+        <Grid>
+          <Row>
+            <Text className="card-text" size={15} weight="bold" color="white">
+              {obj.name}
+            </Text>
+          </Row>
+          {(section !== 'topArtists' && window.innerWidth > 600) &&
+            <Row>
+              <Text h5 className="card-text" size={12} weight="bold" color="white">
+                {obj.artists.map(artist => artist.name).join(', ')}
+              </Text>
+            </Row>
+          }
+        </Grid>
+      </Card.Footer>
+    }
+  </Card>
+
+export default SpotifyCard
--- components/spotify/SpotifySubsection.js
+++ components/spotify/SpotifySubsection.js
@@ -1,5 +1,6 @@
-import { useState, Fragment } from 'react'
-import { Card, Grid, Text, Row, Button, Spacer } from "@nextui-org/react"
+import { Fragment, useState } from 'react'
+import { Button, Grid, Spacer } from "@nextui-org/react"
+import SpotifyCard from './SpotifyCard'
 
 /*
   TODO: reengineer the grid section to take a width variable.
@@ -12,6 +13,8 @@ const SpotifySubsection = ({
   section,
   items
 }) => {
+  const screenWidth = window.innerWidth
+
   const [cardHover, setCardHover] = useState(-1)
   const [showMore, setShowMore] = useState(false)
   const [itemsToShow, setItemsToShow] = useState(items.slice(0, 3))
@@ -30,57 +33,19 @@ const SpotifySubsection = ({
           // because track is nested for recentlyPlayed, get it here
           const obj = section === 'recentlyPlayed' ? item.track : item
 
+          const hovered = cardHover === i
+
           return (
             <Fragment>
-              <Grid xs={4}>
-                <Card
-                  key={i}
-                  className="spotify-card"
-                  cover
-                  hoverable
-                  clickable
-                  onClick={() => window.open(`${obj.external_urls.spotify}`, '_blank')}
+              <Grid xs={6}>
+                <SpotifyCard
+                  i={i}
+                  obj={obj}
+                  hovered={hovered}
+                  section={section}
                   onMouseOver={() => setCardHover(i)}
                   onMouseLeave={() => setCardHover(-1)}
-                >
-                  <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }} />
-                  <Card.Body>
-                    <Card.Image
-                      className='spotify-card-image'
-                      src={obj.album ? obj.album.images[1].url : obj.images[1].url}
-                    />
-                  </Card.Body>
-                  {cardHover === i &&
-                    <Card.Footer
-                      blur
-                      justify="flex-start"
-                      className='spotify-card-footer'
-                      css={{
-                        position: "absolute",
-                        bgBlur: "#0f1114",
-                        borderTop: "$borderWeights$light solid $gray700",
-                        bottom: 0,
-                        zIndex: 1,
-                        padding: '6px !important'
-                      }}
-                    >
-                      <Grid>
-                        <Row>
-                          <Text className="card-text" size={15} weight="bold" color="white">
-                            {obj.name}
-                          </Text>
-                        </Row>
-                        {(section !== 'topArtists' && window.innerWidth > 600) &&
-                          <Row>
-                            <Text h5 className="card-text" size={12} weight="bold" color="white">
-                              {obj.artists.map(artist => artist.name).join(', ')}
-                            </Text>
-                          </Row>
-                        }
-                      </Grid>
-                    </Card.Footer>
-                  }
-                </Card>
+                />
               </Grid>
             </Fragment>
           )})}
